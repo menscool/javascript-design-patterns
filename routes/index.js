f0bb5452afe88f85e1e279ad26d71117e822b248
menscool <menscool@yandex.ru>
2020-12-05 22:39:14 +0300

    Remove next call to escape double headers sending

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
   const files = await fse.readdir(path.resolve("public", "js"));
   const selectedFile = files.find((file) => file === req.query.file);
   res.render("index", { selectedFile, files });
-  next();
 });
 
 module.exports = router;
